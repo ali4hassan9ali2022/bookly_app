3db6b0454206083e19bc054ee14dca2839889653
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2024-12-12 18:38:58 +0200

    create style file & add text to home view

--- lib/Features/Home/presentation/views/Widgets/home_view_body.dart
+++ lib/Features/Home/presentation/views/Widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/Core/utils/styles.dart';
 import 'package:bookly_app/Features/Home/presentation/views/Widgets/custom_app_bar.dart';
 import 'package:bookly_app/Features/Home/presentation/views/Widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -7,11 +8,22 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-        FeaturedBooksListView(),
-      ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            "Best Seller",
+            style: Styles.titleMediun,
+          )
+        ],
+      ),
     );
   }
 }
