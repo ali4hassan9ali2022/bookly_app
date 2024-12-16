077b12d1cd3a2ce12ffe7852cf196cdb43e22a6e
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2024-12-16 17:55:18 +0200

    create similar books list view

--- lib/Features/Home/presentation/views/Widgets/book_details_view_body.dart
+++ lib/Features/Home/presentation/views/Widgets/book_details_view_body.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/Features/Home/presentation/views/Widgets/book_rating.
 import 'package:bookly_app/Features/Home/presentation/views/Widgets/button_action.dart';
 import 'package:bookly_app/Features/Home/presentation/views/Widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/Features/Home/presentation/views/Widgets/custom_book_image.dart';
+import 'package:bookly_app/Features/Home/presentation/views/Widgets/similar_books_list_view.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -48,8 +49,29 @@ class BookDetailsViewBody extends StatelessWidget {
             height: 37,
           ),
           const ButtonAction(),
+          const SizedBox(
+            height: 50,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "You can also like",
+              style: Styles.textStyle14.copyWith(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const SimilarBooksListView(),
+          // const SizedBox(
+          //   height: 40,
+          // ),
         ],
       ),
     );
   }
 }
+
+
