1ecc2fd70ce3f812369bcd331b86918520967fe2
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2025-02-04 17:08:08 +0200

    implement fetch newset books

--- lib/Features/Home/Data/Repo/home_repo.dart
+++ lib/Features/Home/Data/Repo/home_repo.dart
@@ -1,7 +1,8 @@
+import 'package:bookly_app/Core/errors/failure.dart';
 import 'package:bookly_app/Features/Home/Data/Models/book_model/book_model.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<Future, List<BookModel>>> fetchBestSellerBooks();
-  Future<Either<Future, List<BookModel>>> fetchFeatureBooks();
+  Future<Either<Failure, List<BookModel>>> fetchNewsetBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks();
 }
--- lib/Features/Home/Data/Repo/home_repo_impl.dart
+++ lib/Features/Home/Data/Repo/home_repo_impl.dart
@@ -1,16 +1,32 @@
+import 'package:bookly_app/Core/errors/failure.dart';
+import 'package:bookly_app/Core/utils/api_service.dart';
 import 'package:bookly_app/Features/Home/Data/Models/book_model/book_model.dart';
 import 'package:bookly_app/Features/Home/Data/Repo/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Future, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewsetBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              "volumes?Filtering=free-ebooks&Sorting=newest &q=computer science");
+      List<dynamic> dataBooks = data["items"];
+      List<BookModel> books = [];
+      for (var item in dataBooks) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
-  Future<Either<Future, List<BookModel>>> fetchFeatureBooks() {
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() {
     // TODO: implement fetchFeatureBooks
     throw UnimplementedError();
   }
