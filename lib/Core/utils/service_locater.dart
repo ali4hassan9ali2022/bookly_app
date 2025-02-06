f54fd11bfe5643ae2b87ea1ce27a7cef0893679a
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2025-02-06 17:51:13 +0200

    add stepup service locater

---
+++ lib/Core/utils/service_locater.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_app/Core/utils/api_service.dart';
+import 'package:bookly_app/Features/Home/Data/Repo/home_repo_impl.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupServiceLocater() {
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(
+    getIt.get<ApiService>(),
+  ));
+}
